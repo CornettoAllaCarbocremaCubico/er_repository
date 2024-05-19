11f35a14a268ee74c37af2b2ea2be2ff007ca52b
CornettoAllaCarbocremaCubico <lorenzo.crippa.007@gmail.com>
2024-05-19 09:40:35 +0200

    spinacine e allungamento serpente

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ class Frutto:
         self.y = random.randint(0,n_quadretti-1)
         self.posizione = pygame.math.Vector2(self.x, self.y) #Vector2 è una classe con x e y a cui
                                                                 #attribuisco dei valori float
-    
     def disegna_frutta(self):
         #pygame.rect(x,y,l,h)
         frutta = pygame.Rect(self.x*h_quadretto, self.y*h_quadretto, h_quadretto, h_quadretto)
@@ -16,6 +15,21 @@ class Frutto:
         self.x = random.randint(0,n_quadretti-1)
         self.y = random.randint(0,n_quadretti-1)
         self.posizione = pygame.math.Vector2(self.x, self.y)
+
+class Spinacina:
+    def __init__(self):
+        self.x = random.randint(0, n_quadretti - 1)
+        self.y = random.randint(0, n_quadretti - 1)
+        self.posizione = pygame.math.Vector2(self.x, self.y)
+        self.posizionata = True
+    
+    def disegna_spinacina(self):
+        if self.posizionata == True:
+            spinacina = pygame.Rect(self.x * h_quadretto, self.y * h_quadretto, h_quadretto, h_quadretto)
+            pygame.draw.rect(screen, (139, 69, 19), spinacina)
+
+    def rimuovi(self):
+        self.posizionata = False
 
 class Serpente:
     def __init__(self) -> None:
@@ -33,18 +47,17 @@ class Serpente:
         nuovatesta = self.corpo[0] + self.direzione  
         self.corpo.pop()  
         self.corpo.insert(0, nuovatesta)  
-
-    def cambia_direzione(self, nuova_direzione):
-        if nuova_direzione != -self.direzione:
-            self.direzione = nuova_direzione
     
     def collisione(self):
         if self.corpo[0] == frutto.posizione:
             frutto.nuova_posizione()
-            
-
- 
-
+            self.corpo.append(self.corpo[0])
+        if self.corpo[0] == spinacina.posizione:
+            self.corpo.append(self.corpo[0])
+            self.corpo.append(self.corpo[0])
+            self.corpo.append(self.corpo[0])
+            spinacina.rimuovi()
+        
 pygame.init()
 h_quadretto = 40
 n_quadretti = 20
@@ -52,10 +65,14 @@ screen = pygame.display.set_mode((h_quadretto*n_quadretti, h_quadretto*n_quadret
 clock = pygame.time.Clock()
 
 frutto = Frutto()
+spinacina = Spinacina()
 serpente = Serpente()
 
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
+
+GENERA_SPINACINA_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(GENERA_SPINACINA_EVENT, random.randint(5000, 10000))
 
 while True:
     for event in pygame.event.get():
@@ -65,6 +82,9 @@ while True:
         elif event.type == SCREEN_UPDATE:
             serpente.muoviserpente()
             serpente.collisione()
+        elif event.type == GENERA_SPINACINA_EVENT:
+                spinacina.disegna_spinacina()
+                pygame.time.set_timer(GENERA_SPINACINA_EVENT, random.randint(5000, 10000))
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 serpente.direzione = pygame.math.Vector2(0, -1)
@@ -78,5 +98,6 @@ while True:
     screen.fill("Light Green")
     frutto.disegna_frutta()
     serpente.disegna_serpente()
+    spinacina.disegna_spinacina()
     pygame.display.update()
     clock.tick(60)
