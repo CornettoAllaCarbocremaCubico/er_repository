029564666c5f5dc1d5df21486921805e768d345c
CornettoAllaCarbocremaCubico <lorenzo.crippa.007@gmail.com>
2024-05-30 19:02:39 +0200

    messa schermata di vittoria, suoni vittoria e piccole modifiche nel main

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ font = pygame.font.Font("Snakebite-Regular.ttf", 180)
 fontpunteggio = pygame.font.Font(None, 40)
 mixer.init()
 suono_morte = pygame.mixer.Sound('735797__leochavolla__goofy-instrument-jumpscare.wav')
+suono_vittoria = pygame.mixer.Sound("397353__plasterbrain__tada-fanfare-g.flac")
 
 
 def disegna_testo(text, font, color, surface, x, y):
@@ -30,7 +31,7 @@ def disegna_testo(text, font, color, surface, x, y):
 
 def mostra_schermata_di_morte():
     morte_font = pygame.font.Font("DeathMohawk_PERSONAL_USE_ONLY.otf", 155)
-    morte_surf = morte_font.render('GAME OVER', True, (155, 0, 0))
+    morte_surf = morte_font.render('GAME OVER', True, (0, 0, 0))
     morte_rect = morte_surf.get_rect(center = (n_quadretti * h_quadretto // 2, n_quadretti * h_quadretto // 2))
     screen.blit(morte_surf, morte_rect)
     pygame.display.flip()
@@ -38,11 +39,19 @@ def mostra_schermata_di_morte():
     pygame.time.wait(3500) 
     main_menu()
 
+def schermata_vittoria():
+    vittoria_font = pygame.font.Font("Snakebite-Regular.ttf", 175)
+    vittoria_surf = vittoria_font.render('YOU WIN!', True, (0, 0, 0))
+    vittoria_rect = vittoria_surf.get_rect(center = (n_quadretti * h_quadretto // 2, n_quadretti * h_quadretto // 2))
+    screen.blit(vittoria_surf, vittoria_rect)
+    suono_vittoria.play()
+    pygame.display.flip()
+    pygame.time.wait(3500)
+    main_menu()
+
 def main_menu():
     start_button = Bottone((70, 330), 200, 50, "Start")
     quit_button = Bottone((330, 330), 200, 50, "Exit")
-
-    trasparenza_testo = 0
 
     clock = pygame.time.Clock()
 
@@ -59,9 +68,7 @@ def main_menu():
             pygame.quit()
             sys.exit()
 
-        screen.fill((173, 216, 230))
-        if trasparenza_testo < 255:
-            trasparenza_testo += 1
+        screen.fill((173, 216, 230))  
 
         disegna_testo('Snake', font, (0, 0, 0), screen, 125, 100)
         
@@ -82,7 +89,9 @@ def game():
     SCREEN_UPDATE = pygame.USEREVENT
     pygame.time.set_timer(SCREEN_UPDATE, 150)
 
-    while serpente.vivo:
+    vittoria = False
+
+    while serpente.vivo and not vittoria:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -102,18 +111,22 @@ def game():
         serpente.morte()
         spinacina.spawn_spinacina(serpente.corpo)
 
+        if len(serpente.corpo) >= 15:  # Condizione di vittoria
+            vittoria = True
+
         screen.blit(sfondo, (0, 0))
         frutto.disegna_frutta()
         serpente.disegna_serpente()
         spinacina.disegna_spinacina()
         punteggio.disegna_punteggio(serpente.corpo, fontpunteggio)
        
-
         pygame.display.update()
         clock.tick(fps)
 
-    mostra_schermata_di_morte()
-    
+    if vittoria:
+        schermata_vittoria()
+    else:
+        mostra_schermata_di_morte()
 
 if __name__ == '__main__':
     main_menu()
